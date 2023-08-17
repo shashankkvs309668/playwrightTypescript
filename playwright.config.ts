0e7b2dc4d6c1e19b1af5ca5e8b5b35f9b989a0b2
shashankkvs309668 <shashank.kvs140790@gmail.com>
2023-08-17 18:11:07 +0530

    chrome project configurations

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, devices } from '@playwright/test';
  * https://github.com/motdotla/dotenv
  */
 require('dotenv').config({
-    path:'./configurations/.env.QA',
+    path:`./configurations/.env.${process.env.PLUGINENV}`,
     override:true
 });
 
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -40,7 +40,7 @@ export default defineConfig({
       use: { 
         browserName:'chromium',
         channel:'chrome',
-        headless:false,
+        headless:process.env.HEADLESS==='false'?false:true,
         baseURL:process.env.BASEURL,
         launchOptions:{
           args:["--start-maximized"]
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeEach(async({page})=>{
+  await page.goto('');
+})
+
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
